f6a9d23d6634f641abecc66545eb475246dff1e3
Edina_Berkes <berkes.edina@gmail.com>
2016-08-28 19:39:12 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists story;
-create table story (
-    id integer primary key,
-    Story Title text not null,
-    User Story text not null,
-    Acceptance Criteria text not null,
-    Business value integer not null,
-    Estimation (h) float
-);
