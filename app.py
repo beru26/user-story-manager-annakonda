d1c265a3e5680804de984145cdd868d2f6020f62
beru26 <berkes.edina@gmail.com>
2016-08-28 17:03:21 +0200

    edited and return story

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask
-from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/story'):
-    def add_story():
+@app.route('/story')
+def add_story():
+        return render_template('form.html')
+
+
+@app.route('/story/<story_id>')
+def edit_story():
         pass
 
 
-@app.route('/story/<story_id>'):
-    def edit_story():
-        pass
-
-
-@app.route('/and/list'):
-    def product_backlog():
+@app.route('/and/list')
+def product_backlog():
         pass
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
