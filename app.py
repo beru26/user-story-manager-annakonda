9a934852afb9dad7cd68faa085298340eb77acfe
beru26 <berkes.edina@gmail.com>
2016-08-28 19:36:01 +0200

    edited main file

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
-from flask import render_template
+import sqlite3
+from flask import Flask, g, url_for, request, flash, redirect, render_template
+from connect import *
 
 app = Flask(__name__)
 
 
-@app.route('/story')
+@app.route('/story', methods=['GET'])
 def add_story():
-        return render_template('form.html')
+    """Add story"""
+    return render_template('form.html', title='Add story')
+
+
+@app.route('/story', methods=['POST'])
+def new_story():
+    """Add story"""
+    db = get_db()
+    db.execute("""INSERT INTO app (Story title, User story, Acceptance Criteria, Business value, Estimation)
+               VALUES (?, ?, ?, ?, ?)""",
+               [request.form['Story title'], request.form['User story'], request.form['Acceptance Criteria'],
+                request.form['Business value'], request.form['Estimation']])
+    db.commit()
+    return redirect(url_for('list_stories'))
 
 
 @app.route('/story/<story_id>')
@@ -14,9 +28,14 @@ def edit_story():
         pass
 
 
-@app.route('/and/list')
-def product_backlog():
-        pass
+@app.route('/list', methods=['GET'])
+def list_stories():
+    """Show stories"""
+    db = get_db()
+    query = """SELECT * FROM app"""
+    cur = db.execute(query)
+    stories = cur.fetchall()
+    return render_template('list.html', entries=stories)
 
 
 if __name__ == '__main__':
